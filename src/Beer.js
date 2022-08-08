d33aef2f633b705ebcb275546fbc3c12d00c0b38
MarisolM09 <mmendez9980@gmail.com>
2022-08-08 18:26:48 -0500

    Added fontawesome heart like button

--- src/Beer.js
+++ src/Beer.js
@@ -1,19 +1,24 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export function Beer(props) {
-   
+  const [isLiked, setIsLiked] = useState(false);
 
-    return (
+  const handleClick = () => {
+    setIsLiked(!isLiked);
+  };
+
+  return (
     <div>
       <li>
         <h3>{props.name}</h3>
-        <img  className="image" alt={props.name} src={props.image}/>
-        <button></button>
+        <img className="image" alt={props.name} src={props.image} />
+        <button onClick={handleClick}>Like</button>
+        {isLiked && <FontAwesomeIcon icon={faHeart} style={{ color: "red" }} />}
       </li>
     </div>
-    )
-  }
+  );
+}
 
-
-export default Beer
+export default Beer;
